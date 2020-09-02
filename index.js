f2c847f5db3e8759806b61f819d1ec0a88f29d81
vanessuniq <vanessuniq@gmail.com>
2020-09-02 16:54:47 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Add your Circle class here
+class Circle {
+    constructor(radius) {
+        this.radius = radius;
+    };
+    get diameter() {
+        return this.radius * 2;
+    };
+    set diameter(newValue) {
+        if ((typeof newValue) === 'number' && newValue > 0) {
+            this.radius = newValue/2 ;
+        } else {
+            throw new Error(`${newValue} is not a valid value for this measurement`);
+        }
+    };
+
+    get circumference() {
+        return Math.PI * this.diameter;
+    };
+    set circumference(newValue) {
+        if ((typeof newValue) === 'number' && newValue > 0) {
+            this.radius = newValue/(Math.PI * 2) ;
+        } else {
+            throw new Error(`${newValue} is not a valid value for this measurement`);
+        }
+    };
+
+    get area() {
+        return Math.PI * (this.radius)**2;
+    };
+    set area(newValue) {
+        if ((typeof newValue) === 'number' && newValue > 0) {
+            this.radius = Math.sqrt(newValue/Math.PI) ;
+        } else {
+            throw new Error(`${newValue} is not a valid value for this measurement`);
+        }
+    };
+};
